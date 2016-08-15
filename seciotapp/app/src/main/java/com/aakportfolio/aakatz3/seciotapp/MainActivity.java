6e5e02f44d31adbcec8d3203db515a3c931d7cc6
Andrew Katz <aakatz3@gmail.com>
2016-08-14 23:33:05 -0400

    fix app

---
+++ seciotapp/app/src/main/java/com/aakportfolio/aakatz3/seciotapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.aakportfolio.aakatz3.seciotapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
