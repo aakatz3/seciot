57de12e25ccf364e5539755ba4ddec13b27f3133
Andrew Katz <aakatz3@gmail.com>
2016-08-29 12:19:16 -0400

    Change debug port

--- client/seciothome/seciothome/__init__.py
+++ client/seciothome/seciothome/__init__.py
@@ -48,7 +48,7 @@ def main():
 	return page
 
 if __name__ == "__main__":                                                                                                                   
-    app.run(host='0.0.0.0',debug=True, port=8080)                                                                                                                  
+    app.run(host='0.0.0.0',debug=True, port=80)                                                                                                                  
               
                                                                                                                                              
 
