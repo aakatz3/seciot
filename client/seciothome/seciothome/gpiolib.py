018719111b26d176e2193eff1e584eb070088765
Andrew Katz <aakatz3@gmail.com>
2016-09-16 17:17:22 -0400

    fix pins

--- client/seciothome/seciothome/gpiolib.py
+++ client/seciothome/seciothome/gpiolib.py
@@ -6,20 +6,24 @@ import urllib2
 import sqlite3
 import sys
 import RPi.GPIO as GPIO
-import time
 import socket
 from datetime import datetime
 import iotsec_settings
 import seciot
 import json
-
+import time
+import Adafruit_Nokia_LCD as LCD
+import Adafruit_GPIO.SPI as SPI
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 def internet(host="8.8.8.8", port=53, timeout=3):
    """
    Host: 8.8.8.8 (google-public-dns-a.google.com)
    OpenPort: 53/tcp
    Service: domain (DNS/TCP)
-   """"
+   """
    try:
      socket.setdefaulttimeout(timeout)
      socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
@@ -29,7 +33,7 @@ def internet(host="8.8.8.8", port=53, timeout=3):
      return False
 def die():
 	GPIO.output(26,1)
-	time.sleep(10)
+	time.sleep(1)
 	GPIO.cleanup()
 
 def do_reset():
@@ -41,14 +45,15 @@ GPIO.setmode(GPIO.BCM)
 
 #Input Pins
 TAMPER = 4
-RESET = 20
+RESET = 16
 BUTTON = 21
 
 #Output Pins
-BUZ = 24
-BUTTONLED = 25
-LCD_BACKLIGHT = 26
-NET_STATUS = 27
+BUZ = 26
+BUTTON_LED = 17
+LCD_BACKLIGHT = 19
+NET_STATUS = 12
+ERR_LED = 20
 
 #Display pins
 DC = 23
@@ -62,7 +67,7 @@ SPI_DEVICE = 0
 
 for pin in [TAMPER, RESET, BUTTON]:
 	GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-for pin in [BUTTONLED, BUZ, LCD_BACKLIGHT, NET_STATUS]:
+for pin in [BUTTON_LED, BUZ, LCD_BACKLIGHT, NET_STATUS, ERR_LED]:
 	GPIO.setup(pin, GPIO.OUT)
 	GPIO.output(pin,0)
 
@@ -82,13 +87,14 @@ try:
 			GPIO.output(NET_STATUS,0)
 
 		#Tamper Detection
-		if GPIO.input(TAMPER):
+		if GPIO.input(TAMPER) == 0:
 			GPIO.output(BUZ,1)
+			GPIO.output(ERR_LED, 1)
 		else:
 			GPIO.output(BUZ,0)
 		
 		# LCD output
-		if GPIO.input(BUTTON):
+		if GPIO.input(BUTTON) == 1:
 			disp.clear()
 			disp.display()
 			# Create blank image for drawing.
@@ -106,13 +112,16 @@ try:
 			disp.image(image)
 			disp.display()
 			GPIO.output(LCD_BACKLIGHT, 1)
+			GPIO.output(BUTTON_LED, 1)
 		else:
+			GPIO.output(BUTTON_LED, 0)
 			disp.clear()
 			disp.display()
 			GPIO.output(LCD_BACKLIGHT, 0)
 
 		
-		if GPIO.input(RESET):
+		if GPIO.input(RESET) == 1:
+			print "reset"
 			steptime = datetime.now().microsecond + 5000
 			while GPIO.input(RESET) and (steptime < datetime.now().microsecond):
 				time.sleep(0.05)
