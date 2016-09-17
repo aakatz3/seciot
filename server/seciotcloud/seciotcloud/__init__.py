f1992260489e207667740998eb6cf224d8e12fa6
Andrew Katz <aakatz3@gmail.com>
2016-09-17 19:45:21 -0400

    Change cloud

--- server/seciotcloud/seciotcloud/__init__.py
+++ server/seciotcloud/seciotcloud/__init__.py
@@ -1,6 +1,7 @@
 from iotsec_settings import *
-from flask import json
-from flask import Flask,request
+from flask import Flask,request,json
+import os
+import hashlib
 import sqlite3
 
 SQL_DB_PATH = '/var/www/seciotcloud/seciotcloud/iotsec.db'
@@ -8,8 +9,8 @@ SQL_DB_PATH = '/var/www/seciotcloud/seciotcloud/iotsec.db'
 
 
 #import pycrypto
-                                                                                                                                            
-app = Flask(__name__)                                                                                                                        
+																																			
+app = Flask(__name__)																														
 
 
 #
@@ -25,78 +26,94 @@ def check_db():
 	except:
 		conn = sqlite3.connect(SQL_DB_PATH)
 		c = conn.cursor()
-		c.execute("create table iotdata(home_guid text,client_or_server integer,msg_time text,state text,UNIQUE (home_guid,client_or_server)  ON CONFLICT REPLACE);")
+		c.execute("create table iotdata(base_guid text,client_or_server integer,msg_time text,state text, node_map text,UNIQUE (base_guid,client_or_server)  ON CONFLICT REPLACE);") 
 		conn.commit()
 
-
-@app.route("/home/<path>", methods=['GET', 'POST'])                                                                                                                
-def home_service(path, redir = 0):
-	if(redir == 1):
+@app.route("/poll/", methods=['GET', 'POST'])																												
+def poll(override = -1):
+	HOME_OR_MOBILE = request.json['home_or_mobile']
+	print type(HOME_OR_MOBILE)
+	print HOME_OR_MOBILE
+	if(override == -1):
+		print "not redirected"
+	else:
 		print "redirected"
+		HOME_OR_MOBILE = str(override)
 	check_db()
 	conn = sqlite3.connect(SQL_DB_PATH)   
 	c = conn.cursor()
 
-	if 'state' in request.json:
-		c.execute("insert or replace into iotdata (home_guid, client_or_server,msg_time, state) values (?,?,datetime('now','localtime'),?);" , [request.json['guid'], IOT_HOME_NODE, request.get_data()])
-		conn.commit()
-
-#	try:
 	print request.get_data()
-	c.execute("select * from iotdata where home_guid=? and client_or_server=?" , (request.json['guid'], IOT_MOBILE_DEVICE))
-        data = c.fetchone()
-        try:
-                data = data[3]
-        except:
-                data = None
-               
-        if(data == None):
-                if(redir == 0):
-                        mobile_service(redir = 1)
-		else:
-			print "RETURNING NONE!"
-        return data
-#	except:
-#		return json.jsonify([])
+	c.execute("select state from iotdata where base_guid=? and client_or_server=?;" , (request.json['guid'], HOME_OR_MOBILE))
+	data = c.fetchone()
+	print "data:"
+	print type(data)
+	print data
+	
+#	if(not (data is tuple)) and (override == -1):
+#		if (request.json['home_or_mobile'] == IOT_HOME_NODE):
+#			poll(override = IOT_MOBILE_DEVICE)
+#		elif (request.json['home_or_mobile'] == IOT_MOBILE_DEVICE):
+#			poll(override = IOT_HOME_NODE)			   
+#	if not (data is tuple):
+#		print "Data is NONE!"
+#		data = "{}"
+	return data
 
-
-
-
-@app.route("/mobile/<path>", methods=['GET', 'POST'])                                                                                                                
-def mobile_service(path, redir = 0):
-	if(redir == 1):
-		print "redirecting"
+@app.route("/push/", methods=['GET', 'POST'])																												
+def push(redir = 0):
 	check_db()
 	conn = sqlite3.connect(SQL_DB_PATH)   
 	c = conn.cursor()
 
 	if 'state' in request.json:
-		c.execute("insert or replace into iotdata (home_guid, client_or_server,msg_time, state) values (?,?,datetime('now','localtime'),?);" , [request.json['guid'],IOT_MOBILE_DEVICE, request.get_data()])
+		c.execute("insert or replace into iotdata (base_guid, client_or_server,msg_time, state) values (?,?,datetime('now','localtime'),?);" , [request.json['guid'], request.json['home_or_mobile'], request.get_data()])
 		conn.commit()
+	return poll()
 
-#	try:
-	c.execute("select * from iotdata where home_guid=? and client_or_server=?" , [request.json['guid'], IOT_HOME_NODE])
-        data = c.fetchone()
-        try:
-                data = data[3]
-        except:
-                data = None
-               
-        if(data == None):
-                if(redir == 0):
-                        home_service(redir = 1)
-		else:
-			print "RETURNING NONE!"
-        return data
-#	except:
-#		return json.jsonify([])
 
-@app.route("/")                                                                                                                              
-def main():                                                                                                                                  
-    return "Hello, world!!!"                                                                                                                 
 
-if __name__ == "__main__":                                                                                                                   
-    app.run(host="0.0.0.0",port=80,debug=True)                                                                                                                                
-                                                                                                                                             
+@app.route("/nodes/get", methods=['POST'])
+def get_nodes():
+	guid = request.args.get('guid')
+	check_db()
+	conn = sqlite3.connect(SQL_DB_PATH)   
+	c = conn.cursor()
+	c.execute("select node_map from iotdata where base_guid=?;", guid)
+	return c.fetchone()
+
+		
+		
+@app.route("/nodes/set", methods=['POST'])
+def set_nodes():
+	guid = request.json['guid']
+	check_db()
+	conn = sqlite3.connect(SQL_DB_PATH)   
+	c = conn.cursor()
+	if request.method == 'POST':
+		c.execute("insert or replace into iotdata (base_guid, node_map) values (?,?);" , [guid, request.json[node_map]])
+	return get_nodes()
+	
+
+@app.route("/")																															  
+def main():																																  
+	return "Hello, world!!!"																												 
+
+@app.route("/guid/new")
+def new_guid():
+	check_db()
+	conn = sqlite3.connect(SQL_DB_PATH)
+	c = conn.cursor()
+	while True:
+		guid = hashlib.sha1(os.urandom(100)).hexdigest()
+		c.execute("select * from iotdate where base_guid=?;", guid)
+		if(c.fetchone() == None):
+			return guid
+	
+	
+	
+if __name__ == "__main__":																												   
+	app.run(host="0.0.0.0",port=80,debug=True)																																
+																																			 
 
 
